0af78192b99f058c5fa03063861d81d485ef9bcb
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-06-27 18:06:09 -0400

    index creado

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-/* Holiii acá va tu código también */
+//login con alert
+const go = () => { 
+  if (document.form.password.value ==='12345678' &&  document.form.login.value ==='hola@gmail.com'){ 
+    window.open('index2.html') //abre página index si se pone correcto
+    } 
+    else{ 
+      alert("Porfavor ingrese, correo electrónico y contraseña correctos."); 
+    } 
+  }
+
