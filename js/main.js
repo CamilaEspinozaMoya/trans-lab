efab19bbe1cc4cc9e8eeb839fa1c6a5fc4c3d333
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-06-28 11:45:43 -0400

    modificando nodo para agregar tarjeta nueva

--- js/main.js
+++ js/main.js
@@ -10,7 +10,29 @@ const go = () => {
   }
 */
 
+//Perfil
+
+//correo electronico
 const user = document.getElementById("exampleInputEmail1").value;
+const correo = document.getElementById("correo");
+correo.innerHtml = user;
 
+//tarjeta
+//validar que el textarea tenga un comentario
+if(comments.length === 0 || comments === null){
+  alert ('Debes ingresar un mensaje');
+  return false;
+}
 
+//nodos de texto del textarea
+let textNewComment = document.createTextNode(comments);
+
+const contenedorElemento = document.createElement ('p');
+contenedorElemento.appendChild(textNewComment);
+newComments.appendChild(chck);
+newComments.appendChild(trash);
+newComments.appendChild(heart);
+newComments.appendChild(contenedorElemento);
+
+cont.appendChild(newComments);
 
